ea69ad17a319c78e43e84d334c360f85217d1176
Gabriel Aubut-Lussier <dalzhim@hotmail.com>
2017-07-29 15:03:09 -0400

    Documentation + address sanitizer.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  ArticleDynamicFiniteDomain
 //
-//  Created by Gabriel Aubut-Lussier on 2015-07-30.
+//  Created by Gabriel Aubut-Lussier on 2017-07-15.
 //
 
 #include <boost/icl/interval_map.hpp>
@@ -11,10 +11,21 @@
 #include <iostream>
 #include <set>
 
+/**
+ * In real code, we'd expect some payload to be associated to elements of the domain.
+ * This empty struct symbolizes that payload.
+ */
 struct some_payload
 {
 };
 
+/**
+ * This class represents elements of a dynamic domain. An example domain could be the set
+ * of people present inside a room, ordered by a unique number those people were given prior
+ * to entering the room. With this image, insertions and removals can be done at any location
+ * within the set, as illustrated by people entering or leaving the room with whatever unique
+ * number they were holding on to.
+ */
 class dynamic_domain
 {
 public:
@@ -52,12 +63,14 @@ private:
 using container_type = std::map<dynamic_domain::identifier_type, dynamic_domain>;
 using iterator_type = container_type::const_iterator;
 
+/**
+ * This iterator class wraps a std::map<K, V>::const_iterator and adds one more
+ * operator to it : operator<.
+ */
 class dynamic_domain_iterator
 {
 public:
-	dynamic_domain_iterator()
-	: it(g_default_container.begin())
-	{std::cout << g_default_container.size() << '\n';}
+	dynamic_domain_iterator() : it(g_default_container.begin()) {}
 	dynamic_domain_iterator(const iterator_type& it) : it(it) {}
 	
 	const dynamic_domain& operator*() const
@@ -94,16 +107,28 @@ public:
 	
 	bool operator<(const dynamic_domain_iterator& rhs) const
 	{
-		return it->second.get_identifier() < rhs.it->second.get_identifier();
+		return it->second < rhs.it->second;
 	}
 	
 private:
 	iterator_type it;
 	
+	/**
+	 * This default static container makes it possible to default construct an iterator and
+	 * allow it to be incremented twice. This is an optimization that makes it possible to
+	 * default construct empty intervals which can use the general is_empty algorithm of
+	 * Boost.ICL which is : empty = upper < lower. In order for this comparison to be well
+	 * defined, both iterators (upper and lower) must be dereferenced, and thus, must point
+	 * to valid elements.
+	 */
 	const static container_type g_default_container;
 };
 
-const container_type dynamic_domain_iterator::g_default_container{{0x8BADBEEF, dynamic_domain{0x8BADBEEF}}};
+/**
+ * For the interval to be empty, the second element of the container must compare less than the first element.
+ * That is why the second element has a smaller number than the first.
+ */
+const container_type dynamic_domain_iterator::g_default_container{{0x9BADBEEF, dynamic_domain{0x9BADBEEF}}, {0x8BADBEEF, dynamic_domain{0x8BADBEEF}}};
 
 namespace std
 {
@@ -111,13 +136,17 @@ namespace std
 	struct iterator_traits<dynamic_domain_iterator>
 	{
 		using difference_type = iterator_traits<iterator_type>::difference_type;
-		using value_type = iterator_type;
-		using pointer = iterator_type*;
+		using value_type = dynamic_domain_iterator;
+		using pointer = const dynamic_domain_iterator*;
 		using reference = const dynamic_domain_iterator&;
 		using iterator_category = std::bidirectional_iterator_tag;
 	};
 }
 
+/**
+ * This template specialization makes sure that interval containers use
+ * boost::icl::closed_interval when the DomainType is dynamic_domain_iterator.
+ */
 namespace boost { namespace icl {
 	template<>
 	struct interval_type_default<dynamic_domain_iterator>
@@ -126,6 +155,10 @@ namespace boost { namespace icl {
 	};
 }}
 
+/**
+ * Thanks to the interval_type_default specialization, we can simply provide
+ * the DomainType and CodomainType parameters to the template class.
+ */
 using MapType = boost::icl::interval_map<dynamic_domain_iterator, int>;
 
 int main(int argc, const char * argv[])
